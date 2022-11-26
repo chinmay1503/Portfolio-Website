46da8ac40cdcb064237eeb2c143d153375aa3a31
Chinmay Tawde <ctawde@pdx.edu>
2022-11-25 23:04:42 -0800

    Formatted files using Prettier

--- portfolio-app/src/components/Education.js
+++ portfolio-app/src/components/Education.js
@@ -1,25 +1,31 @@
 import React from "react";
 
-const Education = ({duration, degreeName, degreeTitle, universityName, details}) => {
-    return (
-        <div className="card">
-            <div className="row">
-                <div className="col-md-3 bg-primary">
-                <div className="card-body cc-education-header">
-                    <p>{duration}</p>
-                    <div className="h5">{degreeName}</div>
-                </div>
-                </div>
-                <div className="col-md-9">
-                <div className="card-body">
-                    <div className="h5">{degreeTitle}</div>
-                    <p className="category">{universityName}</p>
-                    <p> {details} </p>
-                </div>
-                </div>
-            </div>
+const Education = ({
+  duration,
+  degreeName,
+  degreeTitle,
+  universityName,
+  details,
+}) => {
+  return (
+    <div className="card">
+      <div className="row">
+        <div className="col-md-3 bg-primary">
+          <div className="card-body cc-education-header">
+            <p>{duration}</p>
+            <div className="h5">{degreeName}</div>
+          </div>
         </div>
-    )
-}
+        <div className="col-md-9">
+          <div className="card-body">
+            <div className="h5">{degreeTitle}</div>
+            <p className="category">{universityName}</p>
+            <p> {details} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Education;
